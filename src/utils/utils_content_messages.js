392f9d2c9d54df4a91dc1f85eef2084f9e612c76
DiogoEngh <diogoficial38@gmail.com>
2023-04-06 22:59:48 -0400

    refatoracao das mensagens

---
+++ src/utils/utils_content_messages.js
@@ -0,0 +1,17 @@
+export const templateMessages = {
+  apresentationDepartment: `O DComp dá suporte primordialmente aos cursos de Bacharelado em Ciência da Computação, 
+    Bacharelado em Sistemas de Informação e Bacharelado em Engenharia de Computação, além da oferta de disciplinas 
+    na área de computação a diversos cursos da Universidade.`,
+  about: `O Departamento de Computação (DComp) da Universidade Federal de Sergipe surgiu em 2009. A área de Informática 
+    foi implementada na UFS inicialmente com o curso de graduação em Informática,criado em 1990, junto ao Departamento 
+    de Estatística e Informática (DEI), até o ano de 2009, quando criado o Departamento de Estatística e Ciências 
+    Atuariais e alterada a denominação do DCCE para Departamento de Computação.`,
+};
+
+export const lorem = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda,
+dolor. Veritatis, eum earum! Pariatur, perspiciatis? Nulla, nobis?
+Delectus minus nesciunt, ut doloremque quae commodi quod, beatae
+quibusdam ad, eos vero. Lorem ipsum dolor sit, amet consectetur
+adipisicing elit. Assumenda, dolor. Veritatis, eum earum! Pariatur,
+perspiciatis? Nulla, nobis? Delectus minus nesciunt, ut doloremque quae
+commodi quod, beatae quibusdam ad, eos vero.`;
