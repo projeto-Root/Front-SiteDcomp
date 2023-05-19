22dbc0b95091b4e6f220c0452ffffe0fef19f74f
Igor Fernandes <palusterra@gmail.com>
2023-05-19 16:58:50 -0300

    atualizar pagina representacoes

--- src/views/Representations/Representations.jsx
+++ src/views/Representations/Representations.jsx
@@ -1,41 +1,55 @@
 import React from "react";
-import './MenuRepresentações.css'; 
-import calicomp from '../../assets/LOGOCALICOMPSVG.svg';
-import bugados from '../../assets/BUGADOS1.png';
-import sofTeam from '../../assets/LOGOSOFTEAM.svg';
+import "./MenuRepresentações.css";
+import calicomp from "../../assets/calicomp.png";
+import bugados from "../../assets/bugados.png";
+import softeam from "../../assets/softeam.png";
 
 const Representations = () => {
-    return (
-        <section>
-            <div className="container">
-                <div className="calicomp">
-                    <img src={calicomp} className="images" alt="Logo calicomp" />
-                    <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Debitis atque modi minima facere dolore vero iusto omnis,
-                        voluptas nulla numquam deleniti expedita.
-                        Eligendi temporibus maxime dolorem officiis. Eius, dicta voluptate.</p>
-                    <a href="represents/calicomp"><button className="btn">Saber mais</button></a>
-                </div>
-                <div className="sofTeam">
-                    <img src={sofTeam} className="images" alt="Logo SofTeam" />
-                    <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Debitis atque modi minima facere dolore vero iusto omnis,
-                        voluptas nulla numquam deleniti expedita.
-                        Eligendi temporibus maxime dolorem officiis. Eius, dicta voluptate.</p>
-                    <a href="represents/softeam"><button className="btn">Saber mais</button></a> 
-                </div>
-                <div className="atletica">
-                    <img src={bugados} className="images" alt="Logo atlética" />
-                    <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Debitis atque modi minima facere dolore vero iusto omnis,
-                        voluptas nulla numquam deleniti expedita.
-                        Eligendi temporibus maxime dolorem officiis. Eius, dicta voluptate.</p>
-                    <a href="represents/bugados"><button className="btn">Saber mais</button></a>
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section>
+      <div className="container">
+        <div className="calicomp">
+          <img src={calicomp} className="images" alt="Logo calicomp" />
+          <h1>CALICOMP</h1>
+          <p className="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            atque modi minima facere dolore vero iusto omnis, voluptas nulla
+            numquam deleniti expedita. Eligendi temporibus maxime dolorem
+            officiis. Eius, dicta voluptate.
+          </p>
+          <a href="represents/calicomp">
+            <button className="btn">Saber mais</button>
+          </a>
+        </div>
+        <div className="sofTeam">
+          <img src={softeam} className="images" alt="Logo SofTeam" />
+          <h1>SofTeam</h1>
+          <p className="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            atque modi minima facere dolore vero iusto omnis, voluptas nulla
+            numquam deleniti expedita. Eligendi temporibus maxime dolorem
+            officiis. Eius, dicta voluptate.
+          </p>
+          <a href="represents/softeam">
+            <button className="btn">Saber mais</button>
+          </a>
+        </div>
+        <div className="atletica">
+          <img src={bugados} className="images" alt="Logo atlética" />
+          <h1>Atlética</h1>
+          <p className="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            atque modi minima facere dolore vero iusto omnis, voluptas nulla
+            numquam deleniti expedita. Eligendi temporibus maxime dolorem
+            officiis. Eius, dicta voluptate.
+          </p>
+          <a href="represents/bugados">
+            <button className="btn">Saber mais</button>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-
-export default Representations
+export default Representations;
