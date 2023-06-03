9b000d4338ea33d728be04f5d8aab73cbaa5786d
DiogoEngh <diogoficial38@gmail.com>
2023-06-03 10:21:44 -0300

    correcao nos cards e atribuicao de contato

--- src/utils/utils_represents.js
+++ src/utils/utils_represents.js
@@ -6,19 +6,19 @@ import { lorem } from './utils_content_messages'
 export const dataRepresents = [
     {
         name: 'Calicomp',
-        describe: lorem,
+        describe: 'O CaliComp é uma representação estudantil que foi criada com o intuito de ajudar e representar todos os estudantes que fazem parte dos cursos de computação, onde a mesma sempre estar correndo atrás de trazer novidades para os estudantes, e também sempre correndo atrás dos direitos dos alunos e os representando da melhor forma possível.',
         img: calicomp,
         link: '/represents/calicomp'
     },
     {
         name: 'SofTeam',
-        describe: lorem,
+        describe: 'A SofTeam é a Empresa Júnior de Computação da Universidade Federal de Sergipe, onde a mesma atua no desenvolvimentos de Websites, Sistemas, Aplicativos e até mesmo Jogos, ela foi fundada em 1997, e tem o principal objetivo ajudar estudantes que têm interesse em se aperfeiçoar em suas práticas na programação e com isso também sempre manter o seu grande nome e sempre melhorando na prestação de seus serviços.',
         img: LOGOSOFTEAMSVG,
         link: '/represents/softeam'
     },
     {
         name: 'Atlética',
-        describe: lorem,
+        describe: 'A Associação Atlética Acadêmica de Computação Bugados, foi fundada em agosto de 2022, onde a mesma nasceu com o grande objetivo de fortalecer a comunidade acadêmica e enriquecer a vivência universitária dos estudantes de computação da Universidade Federal de Sergipe, para isso ela acaba promovendo eventos culturais, educacionais e até mesmo recreativos.',
         img: BUGADOS1,
         link: '/represents/bugados'
     }
